48136c32477d508ce06f63b62591074434933028
Anthony DiSanti <anthony.disanti@gmail.com>
2025-02-12 03:27:32 -0500

    Adds linting configuration

---
+++ globals/mw.lua
@@ -0,0 +1,6 @@
+---@class mw  -- Define mw as a class
+---@field text mw.text
+
+---@class mw.text  -- Define mw.text as a class
+---@field jsonDecode fun(jsonString: string): table
+---@field decode fun(data: string): string
